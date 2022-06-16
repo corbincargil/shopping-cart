25f1c2572838c8f05cf0694163081ee278c9020e
corbincargil <corbin.cargil@gmail.com>
2022-06-16 16:00:51 -0500

    added feature where the top of the deck shows when an item picture is hovered over.

--- src/components/FeaturedItems.js
+++ src/components/FeaturedItems.js
@@ -17,7 +17,17 @@ export default function FeaturedItems() {
                     <li 
                         className='featured product' 
                         key={product.id}>
-                        <img src={product.pic} alt={product.name}/>
+                        <div className="flip-container">
+                            <div className="product-picture">
+                                <div className="front">
+                                    <img src={product.pic} alt={product.name}/>
+                                </div>
+                                <div className="back">
+                                    <img src={product.picBack} alt={product.name}/>
+                                </div>
+                            </div>
+                            
+                        </div>
                         <p className="name">{product.name}</p>
                         <p className="description">{product.description}</p>
                         <p className="price">Price: ${product.price}</p>
--- src/util/products.js
+++ src/util/products.js
@@ -1,29 +1,35 @@
 import brophyPic from '../img/products/girlBrophy.webp';
+import brophyPicBack from '../img/products/girlBrophyBack.webp';
 import mcCrankPic from '../img/products/girlMcCrank.webp';
+import mcCrankPicBack from '../img/products/girlMcCrankBack.webp';
 import maltoPic from '../img/products/girlMalto.webp';
+import maltoPicBack from '../img/products/girlMaltoBack.webp';
 import sectionPic from '../img/products/elementSection.webp';
+import sectionPicBack from '../img/products/elementSectionBack.webp';
 
 
 //factory function to product object for each item
-const product = (name, pic, description, type, brand, price, inStock, id, featured=false) => {
-    return { name, pic, description, type, brand, price, inStock, id, featured };
+const product = (name, pic, picBack, description, type, brand, price, inStock, id, featured=false) => {
+    return { name, pic, picBack, description, type, brand, price, inStock, id, featured };
 };
 
 const girlBrophy93 = new product(
     'Girl Brophy 93 Til Deck',
     brophyPic,
+    brophyPicBack,
     '8.25 x 31.75',
     'deck',
     'girl',
     49.98,
     'Yes',
     101,
-    true
+    
 )
 
 const girlMcCrank = new product(
     'Girl McCrank International OG Deck',
     mcCrankPic,
+    mcCrankPicBack,
     '8.25 x 31.75',
     'deck',
     'girl',
@@ -36,6 +42,7 @@ const girlMcCrank = new product(
 const girlMalto = new product(
     'Girl Malto 93 Til Pop Secret Deck',
     maltoPic,
+    maltoPicBack,
     '8.0 x 31.875',
     'deck',
     'girl',
@@ -48,13 +55,14 @@ const girlMalto = new product(
 const elementSection = new product(
     'Element Section Black Deck',
     sectionPic,
+    sectionPicBack,
     '8.0 x 31.75',
     'deck',
     'element',
     56.99,
     'Yes',
     104,
-
+    true,
 )
 
 
