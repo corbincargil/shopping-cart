0db85c867b662079e0c0d80b712f3b5c89f84801
corbincargil <corbin.cargil@gmail.com>
2022-06-21 14:24:51 -0500

    added Cart.js component

---
+++ src/components/Cart.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Cart(props) {
+    return (
+      <div className="cart hidden">
+         <ul>
+            {
+                props.cartItems.map((product) => (
+                <li 
+                    className='product in-cart' 
+                    key={product.id}>
+                        <div className="product-picture">
+                            <img src={product.pic} alt={product.name}/>
+                        </div>
+                    <p className="name">{product.name}</p>
+                    <p className="description">{product.description}</p>
+                    <p className="price">Price: ${product.price}</p>
+                </li>
+            ))
+            }
+            </ul>
+      </div>
+    );
+  }
+
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Cart from "../components/Cart";
 import About from "../pages/About";
 import Shop from "../pages/Shop";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useState } from "react";
 
 export default function Layout() {
+    const [cartItems, setCartItems] = useState([]);
+
     return(
         <div className="layout">
         <BrowserRouter>
             <Header/>
+            <Cart cartItems={cartItems}/>
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="about" element={<About />} />
-                <Route path="Shop" element={<Shop />} />
+                <Route path="Shop" element={<Shop setCartItems={setCartItems}/>} />
             </Routes>
             <Footer/>
         </BrowserRouter>
