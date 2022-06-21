6ec2f8f33972f9e50e11292f3403f0ca2117d344
corbincargil <corbin.cargil@gmail.com>
2022-06-21 16:52:38 -0500

    added initial cart styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,22 +4,24 @@ export default function Cart(props) {
 
 
     return (
-      <div className="cart hidden">
-         <ul>
-            {
-                props.cartItems.map((product) => (
-                <li 
-                    className='product in-cart' 
-                    key={product.id}>
-                        <div className="product-picture">
-                            <img src={product.pic} alt={product.name}/>
-                        </div>
-                    <p className="name">{product.name}</p>
-                    <p className="price">Price: ${product.price}</p>
-                </li>
-            ))
-            }
-            </ul>
+      <div className="cart-background hidden">
+        <div className="cart-container">
+            <ul>
+                {
+                    props.cartItems.map((product) => (
+                    <li 
+                        className='product in-cart' 
+                        key={product.id}>
+                            <div className="product-picture">
+                                <img src={product.pic} alt={product.name}/>
+                            </div>
+                        <p className="name">{product.name}</p>
+                        <p className="price">Price: ${product.price}</p>
+                    </li>
+                ))
+                }
+                </ul>
+            </div>
       </div>
     );
   }
