86de948d176a815f8aee6700150f8b75a6cc0f8d
corbincargil <corbin.cargil@gmail.com>
2022-07-11 17:41:20 -0500

    added a style sheet for the about page. added content to the about.js page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "../styles/about.css"
 
 export default function About() {
     return(
-        <div className="main-container">
-        <h1>About!</h1>
+        <div className="container">
+            <div className="about">
+                <h1>About Skate Warehouse</h1>
+                <p>This is a totally fake website for a totally real company. This fake website is just a project to help me develop my web development skills. The main focus was to practice using functional components and hooks with React.js. All products listed on this fake website are <strong>real</strong> products that can be found on the acutal store <a href="https://www.skatewarehouse.com/">here</a>. </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni odio tenetur facilis minus ipsam. Hic voluptatum at ipsum ullam sit, pariatur facilis suscipit molestias quisquam ab eaque dignissimos labore sed.</p>
+
+                <p>The source code can be found <a href="https://github.com/corbincargil/shopping-cart">here</a>.</p>
+            </div>
         </div>
     )
 }
