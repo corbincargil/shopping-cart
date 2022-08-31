166b36db355cf4187a55806e5042ccc33686bb62
corbincargil <corbin.cargil@gmail.com>
2022-08-31 13:48:06 -0500

    refactored code for Layout and Shop using composition to avoid 'prop drilling'.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,8 @@ import Home from "../pages/Home";
 import Cart from "../components/Cart";
 import About from "../pages/About";
 import Shop from "../pages/Shop";
+import ProductNavigation from "./ProductNavigation";
+import ProductDisplay from "./ProductDisplay";
 import ProductDetail from "../pages/ProductDetail";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -39,18 +41,23 @@ export default function Layout() {
                 <Route path="/shopping-cart/" element={<Home />} />
                 <Route path="/shopping-cart/about" element={<About />} />
                 <Route path="/shopping-cart/shop" element={
-                    <Shop 
-                    cartItems={cartItems} 
-                    setCartItems={setCartItems}
-                    filterBy={filterBy}
-                    setFilterBy={setFilterBy}
-                    filterWord={filterWord}
-                    setFilterWord={setFilterWord}
-                    productsDisplayed={productsDisplayed}
-                    setProductsDisplayed={setProductsDisplayed}
-                    totalCartQuantity={totalCartQuantity}
-                    setTotalCartQuantity={setTotalCartQuantity}
-                />}/> 
+                    <Shop> 
+                        <ProductNavigation 
+                            filterBy={filterBy}
+                            setFilterBy={setFilterBy}
+                            filterWord={filterWord}
+                            setFilterWord={setFilterWord}
+                            setProductsDisplayed={setProductsDisplayed}
+                        />
+                        <ProductDisplay 
+                            productsDisplayed={productsDisplayed}
+                            cartItems={cartItems}
+                            setCartItems={setCartItems}
+                            totalCartQuantity={totalCartQuantity}
+                            setTotalCartQuantity={setTotalCartQuantity}
+                        />
+                    </Shop>}
+                /> 
                 <Route 
                     path="/shopping-cart/shop/:name" 
                     element={<ProductDetail 
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,33 +1,15 @@
-import React from "react";
+import React, { Children } from "react";
 import ProductNavigation from "../components/ProductNavigation";
 import ProductDisplay from "../components/ProductDisplay";
 import { products } from "../util/products";
 import { useEffect } from "react";
 
 
-export default function Shop(props) {
-    const {cartItems, setCartItems, filterBy, setFilterBy, filterWord, setFilterWord, productsDisplayed, setProductsDisplayed, totalCartQuantity, setTotalCartQuantity} = props;
-    
-    // useEffect(() => {
-    //     setTotalCartQuantity(cartItems.length)
-    // }, [cartItems])
+export default function Shop({ children }) {
 
     return(
         <div className="main-shop-container">
-        <ProductNavigation 
-            filterBy={filterBy}
-            setFilterBy={setFilterBy}
-            filterWord={filterWord}
-            setFilterWord={setFilterWord}
-            setProductsDisplayed={setProductsDisplayed}
-        />
-        <ProductDisplay 
-            productsDisplayed={productsDisplayed}
-            cartItems={cartItems}
-            setCartItems={setCartItems}
-            totalCartQuantity={totalCartQuantity}
-            setTotalCartQuantity={setTotalCartQuantity}
-        />
+            {children}
         </div>
     )
 }
